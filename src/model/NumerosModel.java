289c9ae00d666f9e01b44dc7b3df78c314298a0b
JHE060297 <jhoan.aristizabal@uniminuto.edu.co>
2023-03-02 21:22:00 -0500

    Se añade el metodo ShellSort

--- src/model/NumerosModel.java
+++ src/model/NumerosModel.java
@@ -35,6 +35,8 @@ public class NumerosModel {
     escritor.close();
   }
 
+  // Metodo de Ordenamiento Shell 
+
   private void quickSort(
     int[] array,
     int izquierda,
@@ -74,5 +76,25 @@ public class NumerosModel {
 
   public void ordenarNumerosQuickSort(boolean ascendente) {
     quickSort(this.numeros, 0, this.numeros.length - 1, ascendente);
+  }
+
+  // Metodo de Ordenamiento Shell 
+
+  private void shellSort(int[] array, boolean ascendente) {
+    int n = array.length;
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+      for (int i = gap; i < n; i += 1) {
+        int temp = array[i];
+        int j;
+        for (j = i; j >= gap && ((ascendente && array[j - gap] > temp) || (!ascendente && array[j - gap] < temp)); j -= gap) {
+          array[j] = array[j - gap];
+        }
+        array[j] = temp;
+      }
+    }
+  }
+
+  public void ordenarNumerosShellSort(boolean ascendente) {
+    shellSort(this.numeros, ascendente);
   }
 }
